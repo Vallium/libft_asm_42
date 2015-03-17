70075abbf68b481f333b8c70b56e4053ea77268d
Vallium <aalliot@student.42.fr>
2015-03-17 11:45:32 +0100

    norme .h

--- includes/libfts.h
+++ includes/libfts.h
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   libfts.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,12 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef LIBFTS_H
+# define LIBFTS_H
 
 # include <string.h>
 
-/*PART I*/
+/*
+**PART I
+*/
 void				ft_bzero(void *s, size_t n);
 char				*ft_strcat(char *s1, const char *s2);
 int					ft_isalnum(int c);
@@ -27,16 +29,22 @@ int					ft_tolower(int c);
 int					ft_toupper(int c);
 int					ft_puts(const char *str);
 
-/*PART II*/
+/*
+**PART II
+*/
 size_t				ft_strlen(const char *s);
 void				*ft_memset(void *b, int c, size_t len);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
 char				*ft_strdup(const char *s1);
 
-/*PART III*/
+/*
+**PART III
+*/
 void				ft_cat(int fd);
 
-/*BONUS*/
+/*
+**BONUS
+*/
 void				ft_putchar(char c);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr(const char *s);
@@ -44,8 +52,7 @@ void				ft_putstr_fd(const char *s, int fd);
 int					ft_puts_fd(const char *str, int fd);
 char				*ft_strchr(const char *s, int c);
 int					ft_isspace(int c);
-int 				ft_isupper(int c);
-int 				ft_islower(int c);
-
+int					ft_isupper(int c);
+int					ft_islower(int c);
 
 #endif
